234c88a0f3f76c676d9982951fcc840c6da802ee
alexisgraff5 <lex.graff@gmail.com>
2016-10-24 11:37:24 -0600

    completed Step 4: Create a Controller and

---
+++ js/mainCtrl.js
@@ -0,0 +1,3 @@
+angular.module('friendsList').controller('mainCtrl', function($scope){
+  $scope.friends = ['Heather', 'Amber', 'Shelley', 'Erin', 'Kendall', 'Alana', 'Rebecca', 'Ashton', 'Mitch', 'Blake', 'Colton', 'Afton', 'Jacob', 'Sean']
+});
